ab266633e06bcea52086a03da4144a6308b123ed
dunghenry <trandungksnb6930082001>
2023-04-01 11:37:40 +0700

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template lang="">
+    <div>
+        <h1>Header</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Header',
+};
+</script>
+<style lang=""></style>
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import HomePage from '../pages/HomePage.vue';
+const routes = [
+    {
+        path: '/',
+        name: 'HomePage',
+        component: HomePage,
+    },
+];
+
+export default routes;
